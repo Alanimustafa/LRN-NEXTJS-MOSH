f9e6bec18dfb155ee7021056970d92b7e624da8c
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-04 15:18:36 -0500

    Using of the Suspense from React, and adding the fallback to the users and photos pages

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,7 +1,8 @@
 const Spinner = () => {
     return (
-      <div className="flex justify-center items-center h-20">
+      <div className="flex justify-center items-center h-20 ">
         <div className="w-18 h-18 border-4 border-orange-500 border-dashed rounded-full animate-spin"></div>
+        <div className="ml-4 text-xl text-orange-700">Please wait. Loading...</div>
       </div>
     );
   };
