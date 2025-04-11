4393988d2cf3ba13f18744dcc6bf6c84992c809c
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-10 19:36:13 -0500

    Global Error file has been added

---
+++ app/users/error.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+const UserError = () => {
+  return (
+    <div>
+        <h1 className='text-3xl text-red-500'>User Error</h1>
+        <p className='text-xl text-red-300'>Sorry, we could not load the users.</p>
+        <button className='btn btn-secondary' onClick={() => window.location.reload()}>Reload</button>
+        <button className='btn btn-primary' onClick={() => window.history.back()}>Go Back</button>
+    </div>
+  )
+}
+
+export default UserError
