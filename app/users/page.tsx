bf9bce0bd13974d4107ab44c3c30b5df02ccbe12
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-10 20:46:14 -0500

    Global Error file has been added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,46 @@
-import Link from 'next/link'
+export const dynamic = 'force-dynamic';
+
+import Link from 'next/link';
 import UserTable from './UserTable';
 
-interface Props {
-  searchParams : {
-    sortOrder : string
-  }
-}
-const UsersPage = ( {searchParams : { sortOrder }}: Props) => {
-console.log (sortOrder);
-  
+const UsersPage = async ({
+  searchParams,
+}: {
+  searchParams?:  { sortOrder?: string };
+}) => {
+   const sortOrder = searchParams?.sortOrder || 'id';
+
   return (
     <>
-    
-        <div>
-          <h1>UsersPage</h1>
-        </div>
-        <Link href='/users/new' className='btn btn-soft btn-secondary w-40 border-red-400'>New user</Link>
-        <br />
-        <br />
-        <div className="currentUsers">
-        
-          <h2 className='text-2xl bg-#cc3fc-500 border-3 border-wheat-500 rounded'>Current Users List</h2>
-          <h2 className="timerendering bg-maroon-100 text-lg"> 
-            {new Date().toLocaleTimeString()}
-          </h2>
-          <h3 className="userName text-3xl">
-              <UserTable sortOrder = {sortOrder}/>
-          </h3>
-          <br />
-        </div>
-    </>
-  )
-}
+      <div>
+        <h1>Users Page</h1>
+      </div>
 
-export default UsersPage
+      <Link
+        href="/users/new"
+        className="btn btn-soft btn-secondary w-40 border-red-400"
+      >
+        New User
+      </Link>
+
+      <br />
+      <br />
+
+      <div className="currentUsers">
+        <h2 className="text-2xl border-3 border-wheat-500 rounded">
+          Current Users List
+        </h2>
+
+        <h2 className="timerendering bg-maroon-100 text-lg">
+          {new Date().toLocaleTimeString()}
+        </h2>
+
+        <h3 className="userName text-3xl">
+          <UserTable sortOrder={sortOrder} />
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default UsersPage;
