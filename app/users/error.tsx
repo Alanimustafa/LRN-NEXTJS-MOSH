c2fe5b2802f6677d2bc4846f4174ca91cafae17e
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-10 19:41:18 -0500

    Global Error file has been added

--- app/users/error.tsx
+++ app/users/error.tsx
@@ -1,13 +1,18 @@
 "use client"
 import React from 'react'
 
-const UserError = () => {
+interface Props {
+    error : Error
+    };
+const UserError = ( {error}: Props) => {
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center h-screen'>
         <h1 className='text-3xl text-red-500'>User Error</h1>
         <p className='text-xl text-red-300'>Sorry, we could not load the users.</p>
         <button className='btn btn-secondary' onClick={() => window.location.reload()}>Reload</button>
         <button className='btn btn-primary' onClick={() => window.history.back()}>Go Back</button>
+        <p className='text-red-500 bg-yellow-200 w-50 m-10 justify-center flex border-5'>{error.message}</p>
+        <p className='text-red-500 bg-yellow-200 w-50 m-10 justify-center flex border-5'>{error.name}</p>
     </div>
   )
 }
