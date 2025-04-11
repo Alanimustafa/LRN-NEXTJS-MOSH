faaf14718ada3c42ba08dfa5cd5039095690308a
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-10 20:54:18 -0500

    Building APIs - Use of GIT function to return an array of objects

---
+++ app/api/users/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+    return NextResponse.json([
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+        { id: 3, name: "Alice Johnson" },
+        { id: 4, name: "Bob Brown" },
+        { id: 5, name: "Charlie Davis" },
+        { id: 6, name: "Diana Evans" },
+        { id: 7, name: "Ethan Foster" },
+        { id: 8, name: "Fiona Green" },
+        { id: 9, name: "George Harris" },
+        { id: 10, name: "Hannah Ivers" },
+    ]);
+}
