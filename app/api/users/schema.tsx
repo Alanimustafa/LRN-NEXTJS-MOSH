adf74923ab3a25c75e682a9482663b5f7cbcb36c
MustafaAlani <mustafa.kamel.ani@gmail.com>
2025-04-16 14:26:12 -0500

    User Schema - Followers number type has been updated

--- app/api/users/schema.tsx
+++ app/api/users/schema.tsx
@@ -3,7 +3,7 @@ import { z } from "zod";
 const userSchema = z.object({
   email: z.string().email().optional(),
   name: z.string().min(3).max(50),
-  followers: z.number().optional(),
+  followers: z.number().min(3).optional(),
   isActive: z.boolean().default(true),
   registeredAt: z.date().default(() => new Date())
 })
