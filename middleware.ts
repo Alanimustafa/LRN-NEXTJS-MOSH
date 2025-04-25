897c7f094e1e2bc70594aa7e5f9bffddc10f0c98
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-04-25 13:22:35 -0500

    exchange the export middleware function with direct export default from next-auth as a shortcut

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
-import middleware from "next-auth/middleware";
+// THis middleware is used to protect certain routes in a Next.js application using NextAuth.js for authentication and authorization.
+// It checks if the user is authenticated and authorized to access specific routes, such as /users, /products, /admin, /photos, and /upload.    
+
+export { default } from "next-auth/middleware"; // This imports the default middleware from next-auth
 // Middleware to handle authentication and authorization for the /users route
-export default middleware;
+
+// export default middleware;
+// Middleware function to check authentication and authorization
 
 export const config = {
   matcher: [
