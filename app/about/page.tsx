86607185ddee19421665cc3c7c3ae29fe2b9b88f
MustafaAlani <mustafa.kamel.ani@gmail.com>
2025-05-07 22:49:46 -0500

    text added to theabout page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,6 +8,17 @@ export default function about () {
                 <div
                     className="w-[640px] h-[480px] bg-cover bg-center rounded-lg shadow-lg">
                 </div>
+                <pre className="text-amber-500 text-lg font-light">
+                    <code className="text-amber-500 text-lg font-light">
+                        This is a simple about page for the Next.js application. It provides information about the application and its features.
+                        <br />
+                        <br />
+                        The page is styled using Tailwind CSS classes to create a visually appealing layout.
+                        <br />
+                        <br />
+                        You can customize this page further by adding more content, images, or links as needed.
+                    </code>
+                </pre>
             </div>
         </main>
     )
--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
+
 const AuthProvider = ({children}: {children: ReactNode}) => {
   return (
     <>
