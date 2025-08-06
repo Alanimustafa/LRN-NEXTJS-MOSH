52ae04343557b796bf15f975c813ee445d13fc4f
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-08-05 22:07:55 -0500

    Help Page has been added

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 
-export default function about () {
+export default function Help() {
     return (
         <main className="p-[34] font-light">
-            <h1 className="text-3xl text-amber-500">ABOUT PAGE</h1>
+            <h1 className="text-3xl text-amber-500">HELP PAGE</h1>
             <div className="flex items-center justify-center h-screen">
                
                 <pre className="text-amber-500 text-lg font-light">
                     <code className="text-amber-500 text-lg font-light">
-                        This is a simple about page for the Next.js application. It provides information about the application and its features.
+                        This is a simple help page for the Next.js application. It provides assistance and guidance on how to use the application effectively.
                         <br />
                         <br />
                         The page is styled using Tailwind CSS classes to create a visually appealing layout.
@@ -19,5 +19,5 @@ export default function about () {
                 </pre>
             </div>
         </main>
-    )
+    );
 }
--- app/navBar.tsx
+++ app/navBar.tsx
@@ -36,6 +36,7 @@ const NavBar = () => {
               </button>)
               }
               <Link className="px-4 py-2 bg-orange-500 text-black border-4 border-gray-300 rounded-md  hover:bg-black hover:text-orange-500 hover:border-4 hover:border-orange-500 cursor-pointer mr-5" href="http://localhost:3030">Email</Link>
+              <Link href='/help' className="px-4 py-2 bg-orange-500 text-black border-4 border-gray-300 rounded-md  hover:bg-black hover:text-orange-500 hover:border-4 hover:border-orange-500 cursor-pointer mr-5">Help</Link>
         </div>
     </>
   )
